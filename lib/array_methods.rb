d1ab1dacd05261043b7c1672dfd6e9ca26572aa3
Marco McNulty <mmc90@hotmail.co.uk>
2019-06-28 08:15:03 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,5 @@
 def find_element_index(array, value_to_find)
-  array.each do |int|
-    if int == value_to_find
-      return int
-    end
+  array.index(value_to_find)
 end
 
 def find_max_value(array)
